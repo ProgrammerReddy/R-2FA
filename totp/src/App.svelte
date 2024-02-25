8aee2266924525fb2f5c96d0226cdb29654d67ce
RedIsGaming <jaredhofman@protonmail.com>
2024-02-25 21:03:15 +0100

    feat: Setting up Tauri project with Svelte

---
+++ totp/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Greet from "./lib/Greet.svelte";
+</script>
+
+<main class="container">
+  <h1>Welcome to Reddy2FA!</h1>
+
+  <div class="row">
+    <a href="https://github.com/RedIsGaming/Reddy2FA" target="_blank">
+      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+    </a>
+  </div>
+  <p>Click on the logo to see more.</p>
+  <div class="row">
+    <Greet />
+  </div>
+</main>
---
+++ totp/src/main.ts
@@ -0,0 +1,8 @@
+import "./styles.css";
+import App from "./App.svelte";
+
+const app: App = new App({
+  target: document.getElementById("app"),
+});
+
+export default app;
